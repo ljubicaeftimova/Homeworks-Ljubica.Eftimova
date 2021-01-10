ca1a16b056b3e87e1db10578bd0edba5fb2876c8
ljubicaeftimova <ljubica.eftimova@hotmail.com>
2021-01-10 23:22:27 +0100

    H1-chinese zodiac

---
+++ H1animals/H1Animals.js
@@ -0,0 +1,40 @@
+let inputYear= prompt("Please enter a year");
+let convertedYear= parseInt(inputYear);
+let finalResult=((convertedYear-4)%12);
+
+if (finalResult == 0) {
+    alert ("Your chinese zodiac is RAT!");
+}
+if (finalResult == 1) {
+    alert ("Your chinese zodiac is OX!");
+}
+if (finalResult == 2) {
+    alert ("Your chinese zodiac is TIGER!");
+}
+if (finalResult == 3) {
+    alert ("Your chinese zodiac is RABBIT!");
+}
+if (finalResult == 4) {
+    alert ("Your chinese zodiac is DRAGON!");
+}
+if (finalResult == 5) {
+    alert ("Your chinese zodiac is SNAKE!");
+}
+if (finalResult == 6) {
+    alert ("Your chinese zodiac is HORSE!");
+}
+if (finalResult == 7) {
+    alert ("Your chinese zodiac is GOAT!");
+}
+if (finalResult == 8) {
+    alert ("Your chinese zodiac is MONKEY!");
+}
+if (finalResult == 9) {
+    alert ("Your chinese zodiac is ROOSTER!");
+}
+if (finalResult == 10) {
+    alert ("Your chinese zodiac is DOG!");
+}
+if (finalResult == 11) {
+    alert ("Your chinese zodiac is PIG!");
+}
